6802a3fc109392cbae3dd8bc76fba5fbc1fbb5ad
xjx <xjx@qq.com>
2018-11-14 18:01:12 +0800

    xjx

---
+++ src/test/java/com/didispace/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,18 @@
+package com.didispace.hystrixdashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HystrixDashboardApplication.class)
+@SpringBootApplication
+public class HystrixDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
